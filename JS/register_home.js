d1ab7cabe4b4c15048d98279d869b6f18ed3c324
hanntn232 <91482056+hanntn232@users.noreply.github.com>
2021-12-13 00:21:59 +0700

    sửa nav-bar overlay Home.html

--- JS/register_home.js
+++ JS/register_home.js
@@ -1,3 +1,7 @@
+//Remove check navbar mobile khi màn hình vượt 720px
+function removeChecked(){
+    document.getElementById('nav--input').checked = false;
+}
 
 
 //khai báo hàm lưu email xuống local storage
@@ -60,7 +64,7 @@ function timKiemSanPham() {
     var noiDungTimKiem = document.getElementById('search').value;
     noiDungTimKiem = noiDungTimKiem.trim();
     var danhSachTrungKhop = new Array();
-    var danhSachSanPham = JSON.parse(localStorage.getItem('danhSachSanPhamTam'));
+    var danhSachSanPham = JSON.parse(localStorage.getItem('danhSachSanPham'));
     if (kiemTraNoiDungRong(noiDungTimKiem) == false) {
         for (var i = 0; i < danhSachSanPham.length; i++) {
             if (danhSachSanPham[i].ten.toLowerCase().includes(noiDungTimKiem.toLowerCase()) == true) {
